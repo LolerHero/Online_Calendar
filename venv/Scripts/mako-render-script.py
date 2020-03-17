6571eb1f1bbd3858f1aa17e3ab06d18d15185ba8
LolerHero <bloed82@hotmail.de>
2020-03-17 09:15:58 +0100

    Initial Commit

---
+++ venv/Scripts/mako-render-script.py
@@ -0,0 +1,12 @@
+#!"c:\users\jhn\desktop\projects\bs14 ae\block 7\venv\scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'Mako==1.1.1','console_scripts','mako-render'
+__requires__ = 'Mako==1.1.1'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('Mako==1.1.1', 'console_scripts', 'mako-render')()
+    )
