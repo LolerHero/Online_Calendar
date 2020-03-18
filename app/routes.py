b39c1e405f1836a6a32ad686e92297f25f707678
Julian Hansen <bloed82@hotmail.de>
2020-03-18 15:29:36 +0100

    trying to figure out datefields

--- app/routes.py
+++ app/routes.py
@@ -69,11 +69,15 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    events = [
-        {'author': user, 'body': 'Test Event 1', 'date': '2020, 3, 18, 14, 16, 29, 510745'},
-        {'author': user, 'body': 'Test Event 2', 'date': '2020, 3, 18, 14, 16, 29, 510745'}
-    ]
-    return render_template('user.html', user=user, events=events)
+    page = request.args.get('page', 1, type=int)
+    events = user.events.order_by(Event.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=events.next_num) \
+        if events.has_next else None
+    prev_url = url_for('user', username=user.username, page=events.prev_num) \
+        if events.has_prev else None
+    return render_template('user.html', user=user, events=events.items,
+                           next_url=next_url, prev_url=prev_url)
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
