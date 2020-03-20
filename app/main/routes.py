9b89a7aca57384ea825ccf62dd0a1ab40c413b67
Julian Hansen <julian.hansen@epg.com>
2020-03-20 09:30:18 +0100

    Fixed Routing in \main

--- app/main/routes.py
+++ app/main/routes.py
@@ -25,20 +25,23 @@ def before_request():
 def index():
     form = EventForm()
     if form.validate_on_submit():
-        event = Event(body=form.event.data,
-                      date=form.date.data, author=current_user)
+        language = guess_language(form.event.data)
+        if language == 'UNKNOWN' or len(language) > 5:
+            language = ''
+        event = Event(body=form.event.data, author=current_user,
+                      language=language)
         db.session.add(event)
         db.session.commit()
-        flash('Your event is now live!')
-        return redirect(url_for('index'))
+        flash(_('Your event is now live!'))
+        return redirect(url_for('main.index'))
     page = request.args.get('page', 1, type=int)
     events = current_user.followed_events().paginate(
-        page, app.config['EVENTS_PER_PAGE'], False)
-    next_url = url_for('index', page=events.next_num) \
+        page, current_app.config['EVENTS_PER_PAGE'], False)
+    next_url = url_for('main.index', page=events.next_num) \
         if events.has_next else None
-    prev_url = url_for('index', page=events.prev_num) \
+    prev_url = url_for('main.index', page=events.prev_num) \
         if events.has_prev else None
-    return render_template('index.html', title='Home', form=form,
+    return render_template('index.html', title=_('Home'), form=form,
                            events=events.items, next_url=next_url,
                            prev_url=prev_url)
 
@@ -48,13 +51,14 @@ def index():
 def explore():
     page = request.args.get('page', 1, type=int)
     events = Event.query.order_by(Event.timestamp.desc()).paginate(
-        page, app.config['EVENTS_PER_PAGE'], False)
-    next_url = url_for('explore', page=events.next_num) \
+        page, current_app.config['EVENTS_PER_PAGE'], False)
+    next_url = url_for('main.explore', page=events.next_num) \
         if events.has_next else None
-    prev_url = url_for('explore', page=events.prev_num) \
+    prev_url = url_for('main.explore', page=events.prev_num) \
         if events.has_prev else None
-    return render_template("index.html", title='Explore', events=events.items,
-                           next_url=next_url, prev_url=prev_url)
+    return render_template('index.html', title=_('Explore'),
+                           events=events.items, next_url=next_url,
+                           prev_url=prev_url)
 
 
 @bp.route('/user/<username>')
@@ -63,11 +67,11 @@ def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     page = request.args.get('page', 1, type=int)
     events = user.events.order_by(Event.timestamp.desc()).paginate(
-        page, app.config['EVENTS_PER_PAGE'], False)
-    next_url = url_for('user', username=user.username, page=events.next_num) \
-        if events.has_next else None
-    prev_url = url_for('user', username=user.username, page=events.prev_num) \
-        if events.has_prev else None
+        page, current_app.config['EVENTS_PER_PAGE'], False)
+    next_url = url_for('main.user', username=user.username,
+                       page=events.next_num) if events.has_next else None
+    prev_url = url_for('main.user', username=user.username,
+                       page=events.prev_num) if events.has_prev else None
     return render_template('user.html', user=user, events=events.items,
                            next_url=next_url, prev_url=prev_url)
 
@@ -80,12 +84,12 @@ def edit_profile():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
         db.session.commit()
-        flash('Your changes have been saved.')
-        return redirect(url_for('edit_profile'))
+        flash(_('Your changes have been saved.'))
+        return redirect(url_for('main.edit_profile'))
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
-    return render_template('edit_profile.html', title='Edit Profile',
+    return render_template('edit_profile.html', title=_('Edit Profile'),
                            form=form)
 
 
@@ -94,15 +98,15 @@ def edit_profile():
 def follow(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
-        flash('User {} not found.'.format(username))
-        return redirect(url_for('index'))
+        flash(_('User %(username)s not found.', username=username))
+        return redirect(url_for('main.index'))
     if user == current_user:
-        flash('You cannot follow yourself!')
-        return redirect(url_for('user', username=username))
+        flash(_('You cannot follow yourself!'))
+        return redirect(url_for('main.user', username=username))
     current_user.follow(user)
     db.session.commit()
-    flash('You are following {}!'.format(username))
-    return redirect(url_for('user', username=username))
+    flash(_('You are following %(username)s!', username=username))
+    return redirect(url_for('main.user', username=username))
 
 
 @bp.route('/unfollow/<username>')
@@ -110,15 +114,15 @@ def follow(username):
 def unfollow(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
-        flash('User {} not found.'.format(username))
-        return redirect(url_for('index'))
+        flash(_('User %(username)s not found.', username=username))
+        return redirect(url_for('main.index'))
     if user == current_user:
-        flash('You cannot unfollow yourself!')
-        return redirect(url_for('user', username=username))
+        flash(_('You cannot unfollow yourself!'))
+        return redirect(url_for('main.user', username=username))
     current_user.unfollow(user)
     db.session.commit()
-    flash('You are not following {}.'.format(username))
-    return redirect(url_for('user', username=username))
+    flash(_('You are not following %(username)s.', username=username))
+    return redirect(url_for('main.user', username=username))
 
 
 @bp.route('/translate', methods=['POST'])
